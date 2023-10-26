b8d71c429b2e8481b6be4831c307244146a68786
michal.szwaczko <mikey@wirelabs.net>
2023-10-26 14:59:21 +0200

    merge painting into one method

--- jmaps-viewer/src/main/java/net/wirelabs/jmaps/map/MapRenderer.java
+++ jmaps-viewer/src/main/java/net/wirelabs/jmaps/map/MapRenderer.java
@@ -1,6 +1,7 @@
 package net.wirelabs.jmaps.map;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import net.wirelabs.jmaps.map.utils.TileDebugger;
 import net.wirelabs.jmaps.map.cache.Cache;
 import net.wirelabs.jmaps.map.layer.Layer;
@@ -21,6 +22,7 @@ import java.util.List;
  * Created 6/7/23 by Michał Szwaczko (mikey@wirelabs.net)
  * Class responsible for rendering one map 'frame' of tiles on the JPanel's Graphics Context
  */
+@Slf4j
 public class MapRenderer {
 
     private final MapViewer mapViewer;
@@ -48,92 +50,81 @@ public class MapRenderer {
 
     private void drawTiles(final Graphics g, final int zoom, Point topLeftCorner) {
 
-        Layer baseLayer = mapViewer.getBaseLayer();
-        List<Layer> layers = mapViewer.getLayers();
+        int tileSize = mapViewer.getBaseLayer().getTileSize();
 
-        int tileSize = baseLayer.getTileSize();
-        int width = mapViewer.getWidth();
-        int height = mapViewer.getHeight();
-
-        // for layered final image create only when not already created or differs in size
-        if (finalImage == null || finalImage.getHeight() != tileSize || finalImage.getWidth() != tileSize) {
-            finalImage = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_INT_ARGB);
-            finalImageG2D = finalImage.createGraphics();
-        }
+        createOutputCanvasForMultilayerTiles(tileSize);
 
         // calculate the "visible" viewport area in tiles
-        int numWide = width / tileSize + 2;
-        int numHigh = height / tileSize + 2;
+        int viewportTilesWidth = mapViewer.getWidth() / tileSize + 2;
+        int viewportTilesHeight = mapViewer.getHeight() / tileSize + 2;
+
+        int numTilesX = (int) Math.floor(topLeftCorner.getX() / tileSize); // number of tiles in x direction
+        int numTilesY = (int) Math.floor(topLeftCorner.getY() / tileSize); // number of tiles in y direction
 
 
-        // number of tiles in x direction
-        int tpx = (int) Math.floor(topLeftCorner.getX() / tileSize);
-        // number of tiles in y direction
-        int tpy = (int) Math.floor(topLeftCorner.getY() / tileSize);
-
-        // fetch the tiles from the factory and store them in the tiles cache
-        // attach the tileLoadListener
-        for (int x = 0; x <= numWide; x++) {
-            for (int y = 0; y <= numHigh; y++) {
-                int itpx = x + tpx;
-                int itpy = y + tpy;
-
+        for (int x = 0; x <= viewportTilesWidth; x++) {
+            for (int y = 0; y <= viewportTilesHeight; y++) {
+                // tile x/y in map (same as in slippy url)
+                int tileX = x + numTilesX;
+                int tileY = y + numTilesY;
+                // pixel x,y of tile being drawn
+                int px = (tileX * tileSize - topLeftCorner.x);
+                int py = (tileY * tileSize - topLeftCorner.y);
+                Rectangle currentTileBounds = new Rectangle(tileX * tileSize - topLeftCorner.x, tileY * tileSize - topLeftCorner.y, tileSize, tileSize);
                 // only proceed if the specified tile point lies within the area being painted
-                if (g.getClipBounds().intersects(
-                        new Rectangle(itpx * tileSize - topLeftCorner.x, itpy * tileSize - topLeftCorner.y, tileSize, tileSize))) {
-                    // BASE layer
-                    int ox = (itpx * tileSize - topLeftCorner.x);
-                    int oy = (itpy * tileSize - topLeftCorner.y);
-
-
-                    // if single layer, draw normal image onto g
-                    if (layers.size() == 1) {
-                        drawSingleLayerTile(g, zoom, itpx, itpy, ox, oy, baseLayer);
-                    } else {
-                        // multiple layers - make one image from many layers with alpha channel
-                        drawMultipleLayerTile(g, zoom, finalImage, itpx, itpy, ox, oy, layers);
+                if (g.getClipBounds().intersects(currentTileBounds)) {
+                    renderLayers(g, zoom, tileX, tileY, px, py);
+                    if (mapViewer.isDeveloperMode()) {
+                        TileDebugger.drawTileDebugInfo(g, tileSize, tileX, tileY, px, py, zoom);
                     }
 
-
-                if (mapViewer.isDeveloperMode()) {
-                    TileDebugger.drawTileDebugInfo(g, tileSize, itpx, itpy, ox, oy, zoom);
                 }
-
-
-
             }
+        }
+
+
+    }
+
+    private void createOutputCanvasForMultilayerTiles(int tileSize) {
+        if (mapViewer.getLayers().size() > 1 && (finalImage == null || finalImage.getHeight() != tileSize || finalImage.getWidth() != tileSize)) {
+                log.info("Create finalimage");
+                finalImage = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_INT_ARGB);
+                finalImageG2D = finalImage.createGraphics();
         }
     }
 
+    private void renderLayers(Graphics g,  int zoom, int tileX, int tileY, int px, int py) {
 
-}
+        Layer baselayer = mapViewer.getBaseLayer();
+        List<Layer> layers = mapViewer.getLayers();
 
-    private void drawMultipleLayerTile(Graphics g, int zoom, BufferedImage finalImage, int itpx, int itpy, int ox, int oy, List<Layer> layers) {
-
-        finalImageG2D.setBackground(fillColor);
-        finalImageG2D.clearRect(0, 0, finalImage.getWidth(), finalImage.getHeight());
-
-        for (Layer layer: layers) {
-            AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, layer.getOpacity());
-            String tileUrl = layer.createTileUrl(itpx, itpy, zoom +layer.getZoomOffset());
-            BufferedImage b = tileDownloader.getTile(tileUrl);
+        if (layers.size() == 1) {
+            // render single layer map
+            String tileUrl = baselayer.createTileUrl(tileX, tileY, zoom);
+            BufferedImage image = tileDownloader.getTile(tileUrl);
+            // only draw if it is already in cache (went throuh download)
             if (tileDownloader.isTileInCache(tileUrl)) {
-                if (layer.getOpacity() < 1.0f) {
-                    finalImageG2D.setComposite(ac);
-                }
-                finalImageG2D.drawImage( b, 0, 0, null);
+                g.drawImage(image, px, py, null);
             }
-        }
-        // draw final image on g
-        g.drawImage(finalImage, ox, oy, null);
-    }
+        } else {
 
-    private void drawSingleLayerTile(Graphics g, int zoom, int itpx, int itpy, int ox, int oy, Layer baseLayer) {
-        String tileUrl = baseLayer.createTileUrl(itpx, itpy, zoom);
-        BufferedImage b = tileDownloader.getTile(tileUrl);
-        // only draw if it is already in cache (went throuh download)
-        if (tileDownloader.isTileInCache(tileUrl)) {
-            g.drawImage(b, ox, oy, null);
+            // render multiple layers map
+            finalImageG2D.setBackground(fillColor);
+            finalImageG2D.clearRect(0, 0, finalImage.getWidth(), finalImage.getHeight());
+
+            for (Layer layer : layers) {
+                AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, layer.getOpacity());
+                String tileUrl = layer.createTileUrl(tileX, tileY, zoom + layer.getZoomOffset());
+                BufferedImage b = tileDownloader.getTile(tileUrl);
+                if (tileDownloader.isTileInCache(tileUrl)) {
+                    if (layer.getOpacity() < 1.0f) {
+                        finalImageG2D.setComposite(ac);
+                    }
+                    finalImageG2D.drawImage(b, 0, 0, null);
+                }
+            }
+            // draw final image on g
+            g.drawImage(finalImage, px, py, null);
         }
     }
 
