5658c4b6d7826c8546382f0c1ca9bfb2317889b2
mikey <mikey@wirelabs.net>
2025-05-06 20:23:53 +0200

    [ref] drop old derby-db, use current sqlite

--- jmaps-viewer/src/test/java/net/wirelabs/jmaps/map/cache/DBCacheTest.java
+++ jmaps-viewer/src/test/java/net/wirelabs/jmaps/map/cache/DBCacheTest.java
@@ -1,7 +1,6 @@
 package net.wirelabs.jmaps.map.cache;
 
 import lombok.extern.slf4j.Slf4j;
-import net.wirelabs.jmaps.map.Defaults;
 import net.wirelabs.jmaps.map.utils.ImageUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.ThreadUtils;
@@ -13,6 +12,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.*;
@@ -22,6 +22,8 @@ import java.util.List;
 
 import static net.wirelabs.jmaps.TestUtils.cacheCheckExistenceAndExpiration;
 import static net.wirelabs.jmaps.TestUtils.cacheAssertSameData;
+import static net.wirelabs.jmaps.map.Defaults.DEFAULT_CACHE_TIMEOUT;
+import static net.wirelabs.jmaps.map.Defaults.DEFAULT_TILE_CACHE_DB;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -33,53 +35,52 @@ class DBCacheTest {
     private static final String TEST_TILE_URL_1 = "http://a.b.c.pl/a/b/xxx.png";
     private static final String TEST_TILE_URL_2 = "http://a.b.c.pl/a/b/yyy.png";
     private static final String NONEXISTENT_TILE_URL = "nonexistent";
-    private static final Path TEST_TILECACHE_DB = Paths.get("target/test-db");
+    private static final Path TEST_TILE_CACHE_DIR = Paths.get(System.getProperty("java.io.tmpdir"), "test-db");
     private static final Duration TEST_CACHE_TIMEOUT = Duration.ofHours(2);
 
     @BeforeEach
-    void beforeEach() throws IOException {
-            // before each test shutdown any existing testcache instance, and delete its directory
-            try {
-                String path = String.format("jdbc:derby:%s;shutdown=true", TEST_TILECACHE_DB);
-                DriverManager.getConnection(path);
-            } catch (SQLException ex) {
-                // derby generates exception on sucessful close/shutdown (really ;))
-                // the codes for ok shutdown are xj015 and 08006 (and xj004 in this case (no database open) - otherwise, it's a real exception
-                if (!ex.getSQLState().equals("XJ015") && !ex.getSQLState().equals("08006") && !ex.getSQLState().equals("XJ004")) {
-                    throw new IOException("Could not shutdown derby");
-                }
-                FileUtils.deleteDirectory(TEST_TILECACHE_DB.toFile());
-                assertThat(TEST_TILECACHE_DB.toFile()).doesNotExist();
-            }
+    void beforeEach() throws IOException, SQLException {
+
+        String path = String.format("jdbc:sqlite:%s/cache.db", TEST_TILE_CACHE_DIR);
+        DriverManager.getConnection(path).close();
+        File dbFile = new File(String.valueOf(TEST_TILE_CACHE_DIR), "cache.db");
+        FileUtils.delete(dbFile);
+        assertThat(dbFile).doesNotExist();
     }
 
     @BeforeAll
     static void beforeAll() throws IOException {
+        if (!TEST_TILE_CACHE_DIR.toFile().exists()) {
+            Files.createDirectory(TEST_TILE_CACHE_DIR);
+        }
         File imgFile = new File("src/test/resources/tiles/tile.png");
         File imgFileOther = new File("src/test/resources/tiles/tile-other.png");
         testImg1 = ImageIO.read(imgFile);
         testImg2 = ImageIO.read(imgFileOther);
     }
 
+
     @Test
-    void testDefaultCacheInit() throws SQLException {
+    void testDefaultCacheInit() {
+
         DBCache cache = new DBCache();
-        assertThat(cache.getCacheTimeout()).isEqualTo(Defaults.DEFAULT_CACHE_TIMEOUT);
-        assertThat(cache.getBaseDir()).isEqualTo(Defaults.DEFAULT_TILE_CACHE_DB);
-        assertThat(tileCacheTableExists(cache)).isTrue();
+
+        assertThat(cache.getCacheTimeout()).isEqualTo(DEFAULT_CACHE_TIMEOUT);
+        assertThat(cache.getBaseDir()).isEqualTo(DEFAULT_TILE_CACHE_DB);
+
     }
 
     @Test
     void testCustomCacheInit() throws SQLException {
-        DBCache cache = new DBCache(TEST_TILECACHE_DB, TEST_CACHE_TIMEOUT);
+        DBCache cache = new DBCache(TEST_TILE_CACHE_DIR, TEST_CACHE_TIMEOUT);
         assertThat(cache.getCacheTimeout()).isEqualTo(TEST_CACHE_TIMEOUT);
-        assertThat(cache.getBaseDir()).isEqualTo(TEST_TILECACHE_DB);
+        assertThat(cache.getBaseDir()).isEqualTo(TEST_TILE_CACHE_DIR);
         assertThat(tileCacheTableExists(cache)).isTrue();
     }
 
     @Test
     void putAndGetTest() throws SQLException {
-        DBCache cache = new DBCache(TEST_TILECACHE_DB, TEST_CACHE_TIMEOUT);
+        DBCache cache = new DBCache(TEST_TILE_CACHE_DIR, TEST_CACHE_TIMEOUT);
         // put
         cache.put(TEST_TILE_URL_1, testImg1);
         cache.put(TEST_TILE_URL_2, testImg2);
@@ -92,7 +93,7 @@ class DBCacheTest {
         cacheAssertSameData(cache, TEST_TILE_URL_2, testImg2);
 
         // assert different timestamps
-        assertThat(getTimestamp(cache,TEST_TILE_URL_1)).isNotEqualTo(getTimestamp(cache,TEST_TILE_URL_2));
+        assertThat(getTimestamp(cache, TEST_TILE_URL_1)).isNotEqualTo(getTimestamp(cache, TEST_TILE_URL_2));
 
         // now get nonexisting key - should return nonexistent and not-expired  (cache-api)
         cacheCheckExistenceAndExpiration(cache, NONEXISTENT_TILE_URL, false, false);
@@ -103,17 +104,17 @@ class DBCacheTest {
         List<BufferedImage> images = new ArrayList<>();
         List<Long> timestamps = new ArrayList<>();
 
-        DBCache cache = new DBCache(TEST_TILECACHE_DB, Defaults.DEFAULT_CACHE_TIMEOUT);
+        DBCache cache = new DBCache(TEST_TILE_CACHE_DIR, DEFAULT_CACHE_TIMEOUT);
 
 
         // put entry 2 times in a row, with different image - should result in two different timestamps and different images
         cache.put(TEST_TILE_URL_1, testImg1);
         images.add(cache.get(TEST_TILE_URL_1));
-        timestamps.add(getTimestamp(cache,TEST_TILE_URL_1));
+        timestamps.add(getTimestamp(cache, TEST_TILE_URL_1));
 
         cache.put(TEST_TILE_URL_1, testImg2);
         images.add(cache.get(TEST_TILE_URL_1));
-        timestamps.add(getTimestamp(cache,TEST_TILE_URL_1));
+        timestamps.add(getTimestamp(cache, TEST_TILE_URL_1));
 
         // assert images and timestamps differ
         assertThat(ImageUtils.imagesEqual(images.get(0), images.get(1))).isFalse();
@@ -123,7 +124,7 @@ class DBCacheTest {
     @Test
     void shouldCheckIfTileValidWhenCacheNonZeroAndNotCheckWhenZero() throws InterruptedException {
         // cache with default timeout
-        DBCache cache = new DBCache(TEST_TILECACHE_DB, Defaults.DEFAULT_CACHE_TIMEOUT);
+        DBCache cache = new DBCache(TEST_TILE_CACHE_DIR, DEFAULT_CACHE_TIMEOUT);
         // put some tile in - check if it is there and not expired
         cache.put(TEST_TILE_URL_1, testImg1);
         cacheCheckExistenceAndExpiration(cache, TEST_TILE_URL_1, true, false);
@@ -147,11 +148,11 @@ class DBCacheTest {
 
     @Test
     void shouldNotReportKeyExpiredOnNonexistent() {
-        DBCache cache = new DBCache(TEST_TILECACHE_DB, Defaults.DEFAULT_CACHE_TIMEOUT);
+        DBCache cache = new DBCache(TEST_TILE_CACHE_DIR, DEFAULT_CACHE_TIMEOUT);
         assertThat(cache.keyExpired("NONEXISTENT")).isFalse();
     }
 
-    private long getTimestamp(DBCache cache,String key) throws SQLException {
+    private long getTimestamp(DBCache cache, String key) throws SQLException {
         String timeStampQuery = String.format("select TIMESTAMP from TILECACHE where TILEURL='%s'", key);
         try (Statement stmt = cache.getConnection().createStatement(); ResultSet rs = stmt.executeQuery(timeStampQuery)) {
             assertThat(rs.next()).isTrue();
