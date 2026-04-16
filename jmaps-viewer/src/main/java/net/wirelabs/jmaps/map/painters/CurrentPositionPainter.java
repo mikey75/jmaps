559fba1c67bd520a20bb7254ef6f5f285e3174e2
mikey <mikey@wirelabs.net>
2026-04-16 06:24:59 +0200

    [#78] add visible map crs info

--- jmaps-viewer/src/main/java/net/wirelabs/jmaps/map/painters/CurrentPositionPainter.java
+++ jmaps-viewer/src/main/java/net/wirelabs/jmaps/map/painters/CurrentPositionPainter.java
@@ -2,6 +2,7 @@ package net.wirelabs.jmaps.map.painters;
 
 import net.wirelabs.jmaps.map.MapViewer;
 import net.wirelabs.jmaps.map.geo.Coordinate;
+import net.wirelabs.jmaps.map.layer.Layer;
 
 import java.awt.*;
 import java.util.Locale;
@@ -22,8 +23,10 @@ public class CurrentPositionPainter extends TextPrinter {
     @Override
     public void print(Graphics2D graphics, MapViewer mapViewer, int width, int height) {
         // get coordinate under mouse
-        Coordinate c = mapViewer.getCurrentMap().getBaseLayer().pixelToLatLon(mapViewer.getMouseHandler().getCurrentMousePosition(), mapViewer.getZoom());
-        String text = String.format(Locale.US,"Lon: %.4f Lat: %.4f", c.getLongitude(), c.getLatitude());
+        Layer baseLayer = mapViewer.getCurrentMap().getBaseLayer();
+        Coordinate mousePosition = baseLayer.pixelToLatLon(mapViewer.getMouseHandler().getCurrentMousePosition(), mapViewer.getZoom());
+        String crs = baseLayer.getCrs();
+        String text = String.format(Locale.US,"Lon: %.4f Lat: %.4f [%s]", mousePosition.getLongitude(), mousePosition.getLatitude(), crs);
         printText(graphics, width, height, text);
     }
 
